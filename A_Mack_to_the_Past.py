2b17bbf27d2833b0a04a048615695e3e52e52af0
wolfwm <wolfwalder@gmail.com>
2018-11-13 17:58:57 -0200

    funcoes

--- A_Mack_to_the_Past.py
+++ A_Mack_to_the_Past.py
@@ -1,4 +1,5 @@
 from random import *
+import os
 
 def embaralharPergunta (listaResposta):
     listaPergunta = []
@@ -8,7 +9,51 @@ def embaralharPergunta (listaResposta):
             listaPergunta.append(listaResposta[temp])
     return listaPergunta
 
+def exibirParagrafos(lista):
+    print()
+    for x in range(len(lista)):
+        print(lista[x],'\n')
+
+def exibirListaNumerada(lista):
+    print()
+    for x in range(len(lista)):
+        print(x+1,'-',lista[x],'\n')
+
+def clearScreen():
+    print('''\n\n\n\n\n
+    \n\n\n\n\n\n\n\n\n\n
+    \n\n\n\n\n\n\n\n\n\n
+    \n\n\n\n\n\n\n\n\n\n
+    \n\n\n\n\n\n\n\n\n\n
+    \n\n\n\n\n\n\n\n\n\n
+    \n\n\n\n\n\n\n\n\n\n
+    \n\n\n\n\n\n\n\n\n\n''')
+    '''
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+    '''
+
+def validarSN():
+    escolha = input('S/n: ').lower()
+    while escolha != 's' and escolha != 'sim' and escolha != '' and escolha != 'n' and escolha != 'nao' and escolha != 'não':
+        escolha = input('Escolha inválida\nS/n: ')
+    if escolha == 's' or escolha == 'sim' or escolha == '':
+        return False
+    else:
+        return True
+
+def validarProsseguir():
+    input('Pressione ENTER para prosseguir')
+
 resposta1 = ['primeiro','segundo','terceiro','quarto','quinto','sexto']
 pergunta1 = embaralharPergunta(resposta1)
 
-print(pergunta1)
+def main():
+    exibirParagrafos(resposta1)
+    validarProsseguir()
+    clearScreen()
+    exibirListaNumerada(pergunta1)
+
+main()
